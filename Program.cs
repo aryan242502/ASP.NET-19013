bd56ebfc77efad1691ed0882c661f415e4430fc1
aryan242502 <140370185+aryan242502@users.noreply.github.com>
2025-11-23 13:20:43 +0530

    Program.cs

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@ using DoctorAppointmentSystem.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddSingleton<MongoDBService>();
 builder.Services.AddSingleton<DoctorService>();
@@ -20,23 +20,22 @@ builder.Services.AddSession(options =>
 
 var app = builder.Build();
 
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
 
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Login}/{id?}");
-
 
 app.MapControllerRoute(
     name: "admin",
